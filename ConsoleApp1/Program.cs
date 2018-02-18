91d407500ebfc02d2a68c35c87cdd251686614fc
jpaardekooper <p_jazzzp@live.nl>
2018-02-18 21:58:24 +0100

    random console vanger app thanks to david

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,129 +1,148 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace Glubglub
+namespace ConsoleApp3
 {
     class Program
     {
+        static List<string[]> frame = new List<string[]>();
+
+        static bool gameStatus = true;
+        static string hero = "X"; //plauer
+        static string wall = "#"; //wall
+        static string target = "$"; //target
+        static string emptyCell = " ";
+
         static void Main(string[] args)
         {
-            /*
-            1.Maak een nieuw project(console) voor de opdracht verkeer. Bewaar deze voor de rest van de UML Practica
-            2.Programmeer straks het hele diagram uit.Hierbij geldt:
-                a.Je mag voor ieder element zelf bepalen hoe welk karakter erbij hoort.
-                b.Elk element begint met een random kijkrichting(1, 2, 3, 4).Zoek zelf uit hoe je met random getallen werkt.
-                c.Methoden mogen nu leeg blijven.
-            3.Maak in Main een 2D - array van Verkeersobjecten(20x20)
-            4.Schrijf(in de main - klasse) een methode die het array afdrukt met aan de zij-en bovenkant de coördinaten(in mensentaal, dus beginnend bij 1).Lege vakjes(nu nog allemaal) worden weergegeven door ‘_’ Laat het lijken op: 
-            5.Pas de afdrukmethode zo aan dat bij het afdrukken het karakter van het juiste object op het scherm komt, als er zich op die plaats een verkeersobject bevindt.
-            6.Test handmatig door op plekken in het array elementen te plaatsen en dan te kijken of deze ook verschijnen bij het afdrukken.
-            */
+            //blauw is ook grappig
+            Console.ForegroundColor = ConsoleColor.White;
+            frame.Add(new string[] { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", " ", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", "$", " ", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#", "#", "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "X", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "#" });
+            frame.Add(new string[] { "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#" });
 
-            // Two-dimensional array.
-            // The first number indicates the number of columns, the second is the number of rows
-            int[,] grid = new int[20, 20];
-
-            grid[2, 2] = 1; // center
-
-            //filling grid
-            for (int i = 1; i < 4; i++) // fourth row
+            render();
+            while (gameStatus)
             {
-                grid[i, 3] = 1;
+                var keyInfo = Console.ReadKey();
+                moveHero(keyInfo);
+                render();
             }
-            //filling grid
-            for (int i = 0; i < 5; i++) // the last row
+            Console.ReadKey();
+        }
+        static void render()
+        {
+            Console.Clear();
+            for (int x = 0; x < frame.Count; x++)
             {
-                grid[i, 4] = 1;
+                Console.WriteLine(string.Join("", frame[x]));
             }
+        }
 
-            //output for the grid
-            for (int j = 0; j < grid.GetLength(1); j++)
+        static void youWin()
+        {
+            Console.Clear();
+            Console.WriteLine("Yeeeeeeejjj");
+            Console.Beep();
+            gameStatus = false;
+            Console.ReadKey();
+
+        }
+
+        static void moveHero(ConsoleKeyInfo keyInfo)
+        {
+            for (int x = frame.Count - 1; x >= 0; x--)
             {
-                for (int i = 0; i < grid.GetLength(0); i++)
+                for (int y = 0; y < frame[x].Length; y++)
                 {
-                    Console.Write(" " + grid[i, j]);
+                    if (frame[x][y] == hero)
+                    {
+                        if (keyInfo.Key == ConsoleKey.UpArrow)
+                        {
+                            if ((x - 1) >= 0 && frame[x - 1][y] == emptyCell)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x - 1][y] = hero;
+                                return;
+                            }
+                            else if ((x - 1 >= 0 && frame[x - 1][y] == target))
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x - 1][y] = hero;
+                                youWin();
+                                return;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.DownArrow)
+                        {
+                            if ((x + 1) <= (frame.Count - 1) && frame[x + 1][y] == emptyCell)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x + 1][y] = hero;
+                                return;
+                            }
+                            else if ((x + 1) <= frame.Count - 1 && frame[x + 1][y] == target)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x + 1][y] = hero;
+                                youWin();
+                                return;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.LeftArrow)
+                        {
+                            if ((y - 1) >= 0 && frame[x][y - 1] == emptyCell)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x][y - 1] = hero;
+                                return;
+                            }
+                            else if ((y - 1) >= 0 && frame[x][y - 1] == target)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x][y - 1] = hero;
+                                youWin();
+                                return;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.RightArrow)
+                        {
+                            if ((y + 1) <= frame[x].Length - 1 && frame[x][y + 1] == emptyCell)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x][y + 1] = hero;
+                                return;
+                            }
+                            else if ((y + 1) <= (frame[x].Length - 1) && frame[x][y + 1] == target)
+                            {
+                                frame[x][y] = emptyCell;
+                                frame[x][y + 1] = hero;
+                                youWin();
+                                return;
+                            }
+
+                        }
+
+                    }
                 }
-                Console.WriteLine();
             }
         }
     }
-
-    class Verkeersobject
-    {
-        public char karakter { get; set; }
-        public int richting { get; set; }
-    }
-
-    class Dynamisch : Verkeersobject
-    {
-        public int snelheid { get; set; }
-    }
-
-    class Statisch : Verkeersobject
-    {
-        public int leeftijd { get; set; }
-    }
-
-    class Moeder : Dynamisch
-    {
-        public Moeder() : base()
-        {
-            //doe iets
-        }
-
-        public void Roepen()
-        {
-
-        }
-
-        public void Tellen()
-        {
-
-        }
-    }
-
-    class Kind : Dynamisch
-    {
-
-    }
-
-    class Auto : Dynamisch
-    {
-
-    }
-
-    class Fiets : Dynamisch
-    {
-
-    }
-
-    class Brommer : Dynamisch
-    {
-
-    }
-    //parent class fiets
-    class Ligfiets : Fiets
-    {
-        public Ligfiets() : base()
-        {
-            //doe iets
-        }
-    }
-    //parent class fiets
-    class Racefiets : Fiets
-    {
-        public Racefiets() : base()
-        {
-            //doe iets
-        }
-    }
-    // nog één fiets
-
-    class Stopbord : Statisch
-    {
-
-    }
-
-    // nog 2 borden
 }
-
-
