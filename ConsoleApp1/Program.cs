b0b6d7ba0f4909a347a9f97eee8e7e8130a02a1d
jpaardekooper <p_jazzzp@live.nl>
2018-02-18 19:37:15 +0100

    fixed up till opdracht 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,10 +8,192 @@ namespace Glubglub
 
         static void Main(string[] args)
         {
-            int valueColumn = 0;
-            int valueRow = 0;
-            Boolean isInArray = false;
+          
 
+            List<Verkeersobject> verkeersobjecten = new List<Verkeersobject>();
+            Verkeersobject Moeder = new Moeder();
+            Verkeersobject Kind = new Kind();
+            Verkeersobject Auto = new Auto();
+            Verkeersobject Brommer = new Brommer();
+            Verkeersobject Racefiets = new Racefiets();
+            Verkeersobject Ligfiets = new Ligfiets();
+            Verkeersobject Elektrischefiets = new Elektrischefiets();
+            Verkeersobject Stopbord = new Stopbord();
+            Verkeersobject Stoplicht = new Stoplicht();
+            Verkeersobject Voorangsbord = new Voorangsbord();
+            Verkeersobject Thuisbasis = new Thuisbasis();
+            Verkeersobject Zebrapad = new Zebrapad();
+            Verkeersobject Leeg = new Leeg();
+
+         
+            //creating all verkeersobjects
+            for (int i = 0; i <= 32; i++)
+            {
+                //5%
+                if (i > 12)
+                {
+                    verkeersobjecten.Add(Stoplicht);
+                    verkeersobjecten.Add(Brommer);
+                    verkeersobjecten.Add(Voorangsbord);
+                    verkeersobjecten.Add(Moeder);
+                }
+                //3%
+                if (i > 20)
+                {
+                    verkeersobjecten.Add(Stopbord);
+                    verkeersobjecten.Add(Racefiets);
+                    verkeersobjecten.Add(Elektrischefiets);
+                }
+                //2%
+                if (i > 24)
+                {
+                    verkeersobjecten.Add(Zebrapad);
+                    verkeersobjecten.Add(Thuisbasis);
+                    verkeersobjecten.Add(Ligfiets);
+                }
+
+                //verkeersobjecten.Add(Racefiets);
+                verkeersobjecten.Add(Auto);
+                verkeersobjecten.Add(Kind);
+            }
+
+            /*
+            int z = 0;
+
+            Console.WriteLine();
+            Console.WriteLine("Number\tKarakter\tRichting");
+
+            foreach (Verkeersobject v in verkeersobjecten)
+                if (v != null)
+                {
+                    z++;
+                    Console.WriteLine((z) + "\t " + v.Karakter + "\t\t" + v.Richting);
+
+                }
+                else
+                {
+                    Console.WriteLine("List element has null value.");
+                }
+                */
+
+            Verkeersobject[,] gridArray = new Verkeersobject[20, 20];
+       
+
+            for (int fillArray = 0; fillArray < verkeersobjecten.Count; fillArray++)
+            {
+                int row = Utils.row.Next(0, 20);
+                int column = Utils.column.Next(0, 20);
+              
+                gridArray[row, column] = verkeersobjecten[fillArray];
+
+             //   if (gridArray[valueColumn, valueRow] == gridArray[row, column])
+         //       {
+                    //    Console.WriteLine("THIS ITEM IS IN GRID");
+                    //   Console.Write(" karakter is "+ gridArray[row, column].Karakter);
+           //     }
+          //      else
+          //      {
+                    //Console.Write(0);
+          //      }
+            }
+            Console.WriteLine();
+            Console.WriteLine("List.verkeersobjecten (number of created elements): " + verkeersobjecten.Count);
+            Console.WriteLine("empArray.Rank (number of dimensions) = " + gridArray.Rank);
+            Console.WriteLine("empArray.Length (number of elements) = " + gridArray.Length);
+
+            gridArray[0, 0] = verkeersobjecten[1];
+
+            for (int d = 0; d < 20; d++)
+            {
+                if (d == 0)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("  ".PadRight(2) + (d + 1));
+            }
+            Console.WriteLine();
+
+            // int waarde = 0;
+            //looping through column
+            for (int x = 0; x < gridArray.GetLength(0); x++)
+            {
+                if (x < 9)
+                {
+                    Console.Write((x + 1) + "".PadRight(1));
+                }
+                else
+                {
+                    Console.Write((x + 1) + "".PadRight(0));
+                }
+                //looping through rows
+                for (int y = 0; y < gridArray.GetLength(0); y++)
+                {
+                    if (gridArray[x, y] != null)
+                    {
+                        //als we het getal 10 benaderen en het een verkeersobject bevat dan doen, we een spatie extra voor de uitlijning
+                        if (y < 10)
+                        {
+                            Console.Write("  " + gridArray[x, y].Karakter + " ".PadRight(0));
+                        }
+                        else
+                        {
+                            Console.Write(" " + gridArray[x, y].Karakter + "".PadRight(1));
+                        }
+
+                   /*     if (gridArray[valueColumn, valueRow] == gridArray[x, y])
+                        {
+                            isInArray = true;
+                        }
+                        else
+                        {
+                            isInArray = false;
+                        } */
+                    }
+                    else
+                    {
+                        //als we het getal 10 benaderen en het een geen verkeersobject bevat, dan doen we een spatie extra voor de uitlijning
+                        if (y < 10)
+                        {
+                            gridArray[x, y] = Leeg;
+                            Console.Write("  " + gridArray[x, y].Karakter + " ".PadRight(0));
+                          //  Console.Write("  " + gridArray[x, y] + "Q" + " ".PadRight(0));
+                        }
+                        else
+                        {
+                            gridArray[x, y] = Leeg;
+                            Console.Write(" " + gridArray[x, y].Karakter + "".PadRight(1));
+                            //    Console.Write(" " + gridArray[x, y] + "Q" + "".PadRight(1));
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            /*
+            if (isInArray)
+            {
+                Console.WriteLine(gridArray[valueColumn, valueRow].Karakter + "   " + isInArray);
+                if (gridArray[valueColumn, valueRow].Karakter == 'm')
+                {
+                    Console.WriteLine("Ik heb een kind");
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("4. you have selected gridArray[" + valueColumn + "," + valueRow + "] komt er niet in voor probeer opnieuw" + isInArray);
+                Console.WriteLine();
+                //      Console.Clear();
+
+            } */
+
+           
+            Console.WriteLine("verkeers object is " + verkeersobjecten[1]);
+
+             int valueColumn = 0;
+             int valueRow = 0;
+            // Boolean isInArray = false;
+            
             string signal = "A";      // initialize to neutral
             while (signal != "X")      // X indicates stop
             {
@@ -56,177 +238,13 @@ namespace Glubglub
 
                 // Problem. Take action and then log the problem
                 // and then continue on
+                Console.WriteLine("5. this is in gridArray[" + valueColumn + "," + valueRow + "]   " + gridArray[valueColumn, valueRow].Karakter);
                 Console.WriteLine("{0} -- raise Signal!\n", signal);
 
                 //     Console.Clear();
                 //lijst met alle bestaande objecten 
-                List<Verkeersobject> verkeersobjecten = new List<Verkeersobject>();
-                Verkeersobject Moeder = new Moeder();
-                Verkeersobject Kind = new Kind();
-                Verkeersobject Auto = new Auto();
-                Verkeersobject Brommer = new Brommer();
-                Verkeersobject Racefiets = new Racefiets();
-                Verkeersobject Ligfiets = new Ligfiets();
-                Verkeersobject Elektrischefiets = new Elektrischefiets();
-                Verkeersobject Stopbord = new Stopbord();
-                Verkeersobject Stoplicht = new Stoplicht();
-                Verkeersobject Voorangsbord = new Voorangsbord();
-                Verkeersobject Thuisbasis = new Thuisbasis();
-                Verkeersobject Zebrapad = new Zebrapad();
-
-                //creating all verkeersobjects
-                for (int i = 0; i <= 32; i++)
-                {
-                    //5%
-                    if (i > 12)
-                    {
-                        verkeersobjecten.Add(Stoplicht);
-                        verkeersobjecten.Add(Brommer);
-                        verkeersobjecten.Add(Voorangsbord);
-                        verkeersobjecten.Add(Moeder);
-                    }
-                    //3%
-                    if (i > 20)
-                    {
-                        verkeersobjecten.Add(Stopbord);
-                        verkeersobjecten.Add(Racefiets);
-                        verkeersobjecten.Add(Elektrischefiets);
-                    }
-                    //2%
-                    if (i > 24)
-                    {
-                        verkeersobjecten.Add(Zebrapad);
-                        verkeersobjecten.Add(Thuisbasis);
-                        verkeersobjecten.Add(Ligfiets);
-                    }
-
-                    //verkeersobjecten.Add(Racefiets);
-                    verkeersobjecten.Add(Auto);
-                    verkeersobjecten.Add(Kind);
-                }
-
-                /*
-                int z = 0;
-
-                Console.WriteLine();
-                Console.WriteLine("Number\tKarakter\tRichting");
-
-                foreach (Verkeersobject v in verkeersobjecten)
-                    if (v != null)
-                    {
-                        z++;
-                        Console.WriteLine((z) + "\t " + v.Karakter + "\t\t" + v.Richting);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("List element has null value.");
-                    }
-                    */
-
-                Verkeersobject[,] gridArray = new Verkeersobject[20, 20];
-
-                for (int fillArray = 0; fillArray < verkeersobjecten.Count; fillArray++)
-                {
-                    int row = Utils.row.Next(0, 20);
-                    int column = Utils.column.Next(0, 20);
-
-                    gridArray[row, column] = verkeersobjecten[fillArray];
-
-                    if (gridArray[valueColumn, valueRow] == gridArray[row, column])
-                    {
-                        //    Console.WriteLine("THIS ITEM IS IN GRID");
-                        //   Console.Write(" karakter is "+ gridArray[row, column].Karakter);
-                    }
-                    else
-                    {
-                        //Console.Write(0);
-                    }
-                }
-                Console.WriteLine();
-                Console.WriteLine("List.verkeersobjecten (number of created elements): " + verkeersobjecten.Count);
-                Console.WriteLine("empArray.Rank (number of dimensions) = " + gridArray.Rank);
-                Console.WriteLine("empArray.Length (number of elements) = " + gridArray.Length);
-
-                for (int d = 0; d < 20; d++)
-                {
-                    if (d == 0)
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.Write("  ".PadRight(2) + (d + 1));
-                }
-                Console.WriteLine();
-
-                // int waarde = 0;
-                //looping through column
-                for (int x = 0; x < gridArray.GetLength(1); x++)
-                {
-                    if (x < 9)
-                    {
-                        Console.Write((x + 1) + "".PadRight(2));
-                    }
-                    else
-                    {
-                        Console.Write((x + 1) + "".PadRight(1));
-                    }
-                    //looping through rows
-                    for (int y = 0; y < gridArray.GetLength(0); y++)
-                    {
-                        if (gridArray[x, y] != null)
-                        {
-                            //als we het getal 10 benaderen en het een verkeersobject bevat dan doen, we een spatie extra voor de uitlijning
-                            if (y < 10)
-                            {
-                                Console.Write("  " + gridArray[x, y].Karakter + " ".PadRight(0));
-                            }
-                            else
-                            {
-                                Console.Write(" " + gridArray[x, y].Karakter + " ".PadRight(1));
-                            }
-
-                            if (gridArray[valueColumn, valueRow] == gridArray[x, y])
-                            {
-                                isInArray = true;
-                            }
-                            else
-                            {
-                                isInArray = false;
-                            }
-                        }
-                        else
-                        {
-                            //als we het getal 10 benaderen en het een geen verkeersobject bevat, dan doen we een spatie extra voor de uitlijning
-                            if (y < 10)
-                            {
-                                Console.Write("  " + gridArray[x, y] + "-" + " ".PadRight(0));
-                            }
-                            else
-                            {
-                                Console.Write(" " + gridArray[x, y] + "-" + " ".PadRight(1));
-                            }
-                        }
-                    }
-                    Console.WriteLine();
-                }
-                if (isInArray)
-                {
-                    Console.WriteLine(gridArray[valueColumn, valueRow].Karakter + "   " + isInArray);
-                    if(gridArray[valueColumn, valueRow].Karakter == 'm')
-                    {
-                        Console.WriteLine("Ik heb een kind");
-                    }
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine();                  
-                    Console.WriteLine("4. you have selected gridArray[" + valueColumn + "," + valueRow + "] komt er niet in voor probeer opnieuw" + isInArray);
-                    Console.WriteLine();
-                    //      Console.Clear();
-
-                }
-            }
+           
+            } 
         }
 
     }
@@ -373,6 +391,14 @@ namespace Glubglub
         public Zebrapad() : base()
         {
             Karakter = '*';
+        }
+    }
+
+    class Leeg : Statisch
+    {
+        public Leeg() : base()
+        {
+            Karakter = '-';
         }
     }
 }
